204cd3b6386472be95c55b27b47038996a0fed60
SourenP <souren@papazian.info>
2013-11-11 19:32:32 -0500

    Had forgotten to take out a line

--- index.php
+++ index.php
@@ -47,9 +47,7 @@
 					<%liveAmount = 0;%>
 					<% _.each(array, function(obj){ %>
 						<%if (obj.Live == 1){liveAmount += 1 }%>	
-					<% }); %>		
-
-					<% liveAmount  = 10 %>
+					<% }); %>
 
 					<%colors = new Array();%>
 					<% for(i=0; i < liveAmount; i++){%>
@@ -65,7 +63,6 @@
 						<% _.each(array, function(obj){ %>
 							<%if (obj.Live == 1){%>
 								<%makeShape(obj.ID, obj.Armenian, obj.English, obj.Place, obj.Name, obj.NameArm, obj.Date, obj.Votes, idcount, 120, colors[idcount], '#content');%>
-								<%console.log(idcount == liveAmount + 1)%>
 								<% if (idcount == liveAmount + 1 && idcount%4 != 0){%>
 									<% for(i=0; i < idcount%4; i++){%>
 										<%$('#content').append('<div class="vahaksucks" number='+(idcount-i)+'>  </div>');%>
